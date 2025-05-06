b77b16b6a806fda55660d59621c7a8abc771e892
Timothy Woo <twoo@ucsd.edu>
2025-05-06 01:28:00 -0700

    added correlation

--- pyCicero/correlate_peaks.py
+++ pyCicero/correlate_peaks.py
@@ -16,7 +16,43 @@ logging.basicConfig(
 global LOGGER
 LOGGER = logging.getLogger(__name__)
 
-def aggregate_adata(adata, cicero_adata, mapping_dict, aggregate_obs_column = "aggregate_obs_names", names_delim = ",", quiet = True):
+#=================================MAIN Beginning===================================================================
+
+def correlate_peaks(cons, rna_adata, atac_adata, distal_cutoff = 1000, cons_subset_threshold = 0.2, method = "pearson", aggregate = False):
+
+    if aggregate:
+        LOGGER.info("Aggregating rna_adata to match cicero_adata")
+        rna_adata = aggregate_rna_adata(rna_adata, atac_adata)
+        LOGGER.info("Finished Aggregation")
+
+    LOGGER.info("Generating Peak to Links")
+    cons_subset, peak_to_gene = get_peak_to_gene_links(cons, atac_adata, distal_cutoff=distal_cutoff, cons_subset_threshold=cons_subset_threshold)
+    LOGGER.info("Finished generating Peak to Links")
+    atac_mtx, rna_mtx = prepare_matricies(peak_to_gene, rna_adata, atac_adata)
+
+    LOGGER.info(f"Runnign Correlation with {method} correlation")
+    if method == "pearson":
+        correlations = sparse_corr_cols(atac_mtx, rna_mtx)
+    if method == "partial":
+        correlations = sparse_partial_corr_cols(atac_mtx, rna_mtx)
+    LOGGER.info("Finsihed running Correlation")
+
+    LOGGER.info("Adding Links and Correlation to Cons")
+    links_mapping = pd.DataFrame(peak_to_gene, index = ["Gene Name"]).T
+    links_mapping["correlation"] = correlations
+
+    gene_map = links_mapping["Gene Name"].get
+    corr_map = links_mapping["correlation"].get
+
+    cons_subset["Gene"]             = (cons_subset["Peak1"].map(gene_map).fillna(cons_subset["Peak2"].map(gene_map)))
+    cons_subset["RNA_Correlations"] = (cons_subset["Peak1"].map(corr_map).fillna(cons_subset["Peak2"].map(corr_map)))
+    LOGGER.info("Finished")
+
+    return cons_subset
+
+#=================================MAIN END===================================================================
+
+def aggregate_rna_adata(adata, cicero_adata, aggregate_obs_column = "aggregate_obs_names", names_delim = ",", quiet = True):
     raw_index = pd.Series(np.arange(adata.n_obs), index=adata.obs_names)
     rows, cols = [], []
 
@@ -24,11 +60,12 @@ def aggregate_adata(adata, cicero_adata, mapping_dict, aggregate_obs_column = "a
 
         if isinstance(names, str):
             names = names.split(names_delim) #Names is sometimes stored as csv's in my case to be able to be serialized with adata
-        for n in names:
-            raw_name = mapping_dict.get(n) #Mapping from atac barcodes to rna barcodes (if its the same you should provide a mapping dict that is 1:1)
-            if raw_name is None or raw_name not in adata.obs_names:   # skip missing names
+        for name in names:
+
+            if name not in adata.obs_names:   # skip missing names
                 continue
-            cols.append(raw_index[raw_name])       # raw‑cell (column) index
+            
+            cols.append(raw_index[name])       # raw‑cell (column) index
             rows.append(agg_i)                     # aggregate (row) index
 
     data = np.ones(len(rows), dtype=np.uint8)      # just "count 1"
@@ -38,72 +75,54 @@ def aggregate_adata(adata, cicero_adata, mapping_dict, aggregate_obs_column = "a
         )
 
     aggregated_counts = A @ adata.X            # (n_agg  ×  n_genes) CSR
-    mapped_obs_names = list(map(mapping_dict.get, cicero_adata.obs_names))
-    obs = adata.obs.loc[mapped_obs_names,:] #again were just going to put the obs of the cell center cluster please dont trust this obs
+    obs = adata.obs.loc[cicero_adata.obs_names,:] #again were just going to put the obs of the cell center cluster please dont trust this obs
     var = adata.var
     aggregated_adata = sc.AnnData(aggregated_counts, obs = obs, var = var)
     
     return aggregated_adata
 
-def subset_cicero_window(var, window_chromsome_name, window_start, window_end):
-    return (var["Chromosome"] == window_chromsome_name) & (var["Start"] >= window_start) & (var["End"] <= window_end) 
+def get_peak_to_gene_links(cons, atac_adata, distal_cutoff = 1000, cons_subset_threshold = 0.2):
 
-def generate_matricies(cons, aggregated_adata, cicero_adata, quiet = True):
+    cons_subset = cons[np.abs(cons["coaccess_score"]) >= cons_subset_threshold]
 
-    # cicero_adata_column_indicies = np.zeros(cons.shape[0])
-    # adata_column_indicies = np.zeros(cons.shape[0])
+    #optional to add. Keeping for now
+    atac_adata.var["Proximal|Distal"] = np.where(atac_adata.var["Distance to TSS"] > distal_cutoff,"Proximal","Distal")
+    atac_adata.var["is_proximal"] = np.abs(atac_adata.var["Distance to TSS"]) <= distal_cutoff
+    
+    peak1_annotations = atac_adata.var.loc[cons_subset["Peak1"],:]
+    peak2_annotations = atac_adata.var.loc[cons_subset["Peak2"],:]
 
-    # for index, (row_index, row) in tqdm(enumerate(cons.iterrows()), total = cons.shape[0], desc = "Generating matrix indicies", disable = quiet):
-    #     cicero_adata_column_indicies[index] = np.where(subset_cicero_window(cicero_adata.var, row["Peak1_Chromosome"], row["Peak1_Start"], row["Peak1_End"]))[0][0]
-    #     gene = row["Peak_1_Gene_Annotation"]
-    #     if ";" in gene: #TODO FIX ANNOTATION SO THAT IT DUPES ROWS 
-    #         gene = gene.split(";")[0]
-    #     if gene not in aggregated_adata.var_names:
-    #         adata_column_indicies[index] = aggregated_adata.n_vars
-    #     else:
-    #         adata_column_indicies[index] = np.where(aggregated_adata.var_names == gene)[0][0]
+    #we only analyze the proximal to distal (or distal to proxmial) connections 
+    proximal_distal_mask = np.logical_xor(peak1_annotations["is_proximal"].values, peak2_annotations["is_proximal"].values)
+    
+    peak1_annotations = peak1_annotations[proximal_distal_mask & peak1_annotations["is_proximal"].values]
+    peak2_annotations = peak2_annotations[proximal_distal_mask & peak2_annotations["is_proximal"].values]
 
-    ivals  = pd.IntervalIndex.from_arrays(cicero_adata.var.Start, cicero_adata.var.End, closed="both")
-    cicero_adata.var["ival"] = ivals
-    lookup = cicero_adata.var.set_index(["Chromosome", "ival"]).index
-    cicero_adata_column_indicies = (pd.MultiIndex.from_arrays([cons.Peak1_Chromosome, pd.IntervalIndex.from_arrays(cons.Peak1_Start, cons.Peak1_End,closed="both")]).map(dict(zip(lookup, np.arange(len(lookup))))).to_numpy())
+    return cons_subset[proximal_distal_mask].copy(), peak1_annotations["Gene Name"].to_dict() | peak2_annotations["Gene Name"].to_dict() #merge two dicts
 
-    genes = cons["Peak_1_Gene_Annotation"].str.split(";").str[0]
-    gene_idx = aggregated_adata.var_names.get_indexer(genes)
-    missing_mask = gene_idx == -1
-    gene_idx[missing_mask] = aggregated_adata.n_vars
-    adata_column_indicies = gene_idx.astype(np.int64)
+def prepare_matricies(peak_to_gene, rna_adata, atac_adata,):
+    
+    if np.any(~rna_adata.obs_names.isin(atac_adata.obs_names)) or np.any(~atac_adata.obs_names.isin(rna_adata.obs_names)):
+        LOGGER.warning("rna and atac adata do not share the same obs_names. Trying to fix by computing union of cell names")
+        total_obs = len(rna_adata.obs_names.union(atac_adata.obs_names))
+        obs_names_intersection = rna_adata.obs_names.intersection(atac_adata.obs_names)
+        rna_adata = rna_adata[obs_names_intersection,]
+        atac_adata = atac_adata[obs_names_intersection,]
+        LOGGER.warning(f"Subsetting to {len(obs_names_intersection)/total_obs*100:.2f}% of original observations")
 
-    aggregated_counts = aggregated_adata.X
-    zero_col = csr_matrix((aggregated_counts.shape[0], 1), dtype=aggregated_counts.dtype)
-    rna_counts = hstack([aggregated_counts, zero_col], format="csr")
-    atac_counts = cicero_adata.X
+    peaks = list(peak_to_gene.keys())
+    genes = list(peak_to_gene.values())
 
-    atac_peak_counts = atac_counts[:,cicero_adata_column_indicies]
-    rna_peak_counts  = rna_counts[:,adata_column_indicies]
+    peaks_idx = atac_adata.var_names.get_indexer(peaks)
+    genes_idx = rna_adata.var_names.get_indexer(genes)
 
-    return rna_peak_counts, atac_peak_counts
+    atac_mtx = atac_adata.X[:,peaks_idx]
+    rna_mtx = rna_adata.X[:,genes_idx]
 
-def correlate_peaks(cons, aggregated_adata, cicero_adata, annotate = True, method = "pearson"):
+    return atac_mtx, rna_mtx
 
-    rna_peak_counts, atac_peak_counts = generate_matricies(cons, aggregated_adata, cicero_adata)
 
-    if method == "partial":
-        LOGGER.info("Calculating Partial Correlations")
-        correlation_coefficients = sparse_partial_corr_cols(rna_peak_counts, atac_peak_counts)
-    else:
-        LOGGER.info("Calculating Pearson Correlations")
-        # correlation_coefficients, p_vals = sparse_corr_cols(rna_peak_counts, atac_peak_counts)
-        correlation_coefficients = sparse_corr_cols(rna_peak_counts, atac_peak_counts)
-
-    if annotate and method == "partial":
-        LOGGER.warning("Cannot annotate cons for correlate_peaks if method='Partial'. Partial is experimental. Returning Correlation Vector Directly")
-        # return correlation_coefficients, p_vals
-        return correlation_coefficients
-    else:
-        cons["Correlation Coefficient"] = correlation_coefficients
-        # cons["Correlation P-val"] = p_vals
-        return cons
+#=========================Correlation Functions==============================
 
 def sparse_corr_cols(A, B):
 
@@ -175,25 +194,3 @@ def sparse_corr_cols_gpu(A, B):
     num   = n_row * sAB  - sA * sB
     denom = cp.sqrt((n_row * sA2 - sA ** 2) * (n_row * sB2 - sB ** 2))
     return (num / denom).get()
-
-#===================================UTIL===================================================================================
-def normalize_peak_orientation(cons):
-    peak1_cols = [
-        "Peak1_Original", "Peak1_Chromosome", "Peak1_Start", "Peak1_End",
-        "Peak1_Mean", "Peak1_Closest_TSS", "Peak1_Distal_Proximal_Annotation",
-        "Peak_1_Gene_Annotation"
-    ]
-    peak2_cols = [
-        "Peak2_Original", "Peak2_Chromosome", "Peak2_Start", "Peak2_End",
-        "Peak2_Mean", "Peak2_Closest_TSS", "Peak2_Distal_Proximal_Annotation",
-        "Peak_2_Gene_Annotation"
-    ]
-
-    mask = cons["Linkage_Type"] == "Proximal to Distal"
-    if mask.any():
-        cons.loc[mask, peak1_cols + peak2_cols] = (
-            cons.loc[mask, peak2_cols + peak1_cols].values
-        )
-        cons.loc[mask, "Linkage_Type"] = "Distal to Proximal"
-
-    return cons
